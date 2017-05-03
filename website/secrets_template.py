ae70f6675d7c41b81133555bbb311be4b58195c4
Nick Wilde <Nick Wilde>
2017-05-02 21:45:21 -0600

    initial commit

---
+++ website/secrets_template.py
@@ -0,0 +1,11 @@
+#/usr/bin/python
+
+#use this file to declare important information. copy this file and save it as secrets.py.
+
+DB_HOST = ''
+DB_PORT = ''
+DB_NAME = ''
+DB_USERNAME = ''
+DB_PASSWORD = ''
+
+SECRET_KEY = ''
