ff2d3dd7bc65784e5f07a3245301d8dc50c8548c
nickjwilde <nickjwilde@gmail.com>
2017-05-24 07:45:44 -0600

    add skills on about page

--- nickjwilde/static/nickjwilde/js/main.js
+++ nickjwilde/static/nickjwilde/js/main.js
@@ -1,19 +1,12 @@
 $(document).ready(function () {
    $(".stacked-menu").click(function(e){
         e.preventDefault();
-        if($(".nav-item").hasClass("visible")){
-            $(".nav-item").removeClass("visible");
-        }else{
-            $(".nav-item").addClass("visible");
-        }
+        $(".nav-item").toggleClass("visible");
    });
 
    $(window).resize(function() {
         if($(window).width() > 570){
            $(".nav-item").addClass("visible");
-        }
-        else{
-           $(".nav-item").removeClass("visible");
         }
    });
 });
