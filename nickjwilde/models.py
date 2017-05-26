6d3a66476b7e27fdc60aea1d58dc23da626e04b9
nickjwilde <nickjwilde@gmail.com>
2017-05-25 20:42:00 -0600

    add Skill model for about section. Modify about.html to iterate over skills.

--- nickjwilde/models.py
+++ nickjwilde/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Skill(models.Model):
+    num_years = models.IntegerField()
+    image_name = models.CharField(max_length=255)
+
+    def num_years_text(self):
+        return "".join(str(self.num_years) + ' years' if self.num_years > 1 else str(self.num_years) + ' year')
+
+    def __str__(self):
+        return self.image_name.split('.')[0]
--- nickjwilde/views.py
+++ nickjwilde/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+from .models import Skill
+
 # Create your views here.
 def home(request):
-   return render(request, 'nickjwilde/home.html', None)
+    return render(request, 'nickjwilde/home.html', None)
 
 def about(request):
-   return render(request, 'nickjwilde/about.html', None)
+    skills = Skill.objects.all().order_by('-num_years')
+    context = {'skills': skills}
+    return render(request, 'nickjwilde/about.html', context)
